43ee15a54b692ef56796b34ccefff0756c966a9d
yunnysunny <yunnysunny@gmail.com>
2022-02-12 10:08:11 +0800

    upload benchmark result to artifact

--- benchmark/jmeter/entrypoint.sh
+++ benchmark/jmeter/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 JMETER="/opt/apache-jmeter-5.4.1/bin/jmeter.sh"
-RESULT=/tmp/result/${FRAMEWORK}_result.jtl
-HTML=/tmp/reporter/${FRAMEWORK}
+RESULT=/tmp/result/result.jtl
+HTML=/tmp/reporter/
 PARAM_BENCH="-n -t /config/http.jmx \
 -l  ${RESULT} \
--e -o /tmp/output/${FRAMEWORK}"
+-e -o /tmp/output/"
 echo run "$PARAM_BENCH"
 $JMETER $PARAM_BENCH
 
