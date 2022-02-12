104477f671d35f9e09e1fa1a025fe876746bd385
yunnysunny <yunnysunny@gmail.com>
2022-02-12 08:33:36 +0800

    fix benchmark action not working error

--- benchmark/jmeter/entrypoint.sh
+++ benchmark/jmeter/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 JMETER="/opt/apache-jmeter-5.4.1/bin/jmeter.sh"
-RESULT=/tmp/result/${FRAMEWORK}/result.jtl
-HTML=/tmp/reporter/${FRAMEWORK}.html
+RESULT=/tmp/result/${FRAMEWORK}_result.jtl
+HTML=/tmp/reporter/
 PARAM_BENCH="-n -t /config/http.jmx \
 -l  ${RESULT} \
 -e -o /tmp/output/${FRAMEWORK}"
-
-$JMETER "$PARAM_BENCH"
+echo run "$PARAM_BENCH"
+$JMETER $PARAM_BENCH
 
 $JMETER -g "$RESULT" -e -o "$HTML"
