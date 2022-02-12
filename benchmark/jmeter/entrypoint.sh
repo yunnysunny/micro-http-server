1d9d2605d499442763cd79dd646d693510da78ef
yunnysunny <yunnysunny@gmail.com>
2022-02-12 09:39:44 +0800

    add log for generate html

--- benchmark/jmeter/entrypoint.sh
+++ benchmark/jmeter/entrypoint.sh
@@ -2,7 +2,7 @@
 
 JMETER="/opt/apache-jmeter-5.4.1/bin/jmeter.sh"
 RESULT=/tmp/result/${FRAMEWORK}_result.jtl
-HTML=/tmp/reporter/
+HTML=/tmp/reporter/${FRAMEWORK}
 PARAM_BENCH="-n -t /config/http.jmx \
 -l  ${RESULT} \
 -e -o /tmp/output/${FRAMEWORK}"
@@ -10,3 +10,4 @@ echo run "$PARAM_BENCH"
 $JMETER $PARAM_BENCH
 
 $JMETER -g "$RESULT" -e -o "$HTML"
+echo generate html finished
