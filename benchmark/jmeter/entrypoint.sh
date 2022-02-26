1bf3b6c2bdf286df7c49cd6db8023d1ccbb5c883
yunnysunny <yunnysunny@gmail.com>
2022-02-26 12:20:49 +0800

    ci: create benchmark result in different dir

--- benchmark/jmeter/entrypoint.sh
+++ benchmark/jmeter/entrypoint.sh
@@ -2,13 +2,13 @@
 mkdir -p /tmp/result/ /tmp/output/
 JMETER="/opt/apache-jmeter-5.4.1/bin/jmeter.sh"
 RESULT=/tmp/result/${FRAMEWORK}-result.jtl
-HTML=/tmp/reporter/
+HTML=/tmp/reporter/${FRAMEWORK}
 PARAM_BENCH="-n -t /config/http.jmx \
 -l  ${RESULT} \
 -e -o /tmp/output/"
 echo run "$PARAM_BENCH"
 $JMETER $PARAM_BENCH
 
-$JMETER -g "$RESULT" -e -o $HTML
-ls $HTML
+$JMETER -g "$RESULT" -e -o "$HTML"
+ls "$HTML"
 echo generate html finished
